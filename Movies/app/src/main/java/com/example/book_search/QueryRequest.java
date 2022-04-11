c07e97a106b353e64498369b61e385017c9c7288
TImothy Beck <Timothy_Beck@student.uml.edu>
2022-04-11 15:21:14 -0400

    Query 1
    Should have query 1 done.

---
+++ Movies/app/src/main/java/com/example/book_search/QueryRequest.java
@@ -0,0 +1,32 @@
+package com.example.book_search;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QueryRequest extends StringRequest {
+    private Map<String, String> args;
+    private static Response.ErrorListener err = new Response.ErrorListener(){
+        @Override
+        public void onErrorResponse(VolleyError error){
+            Log.d("please","Error listener response: " + error.getMessage());
+        }
+    };
+
+    public QueryRequest(String input, String url, Response.Listener<String> listener){
+        super(Method.POST, url, listener, err);
+        args = new HashMap<String, String>();
+        args.put("input", input);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return args;
+    }
+}
